d523be3f1cd50873c01ca697708009a5f9dd6789
massi <massinissa.aitaba@gmail.com>
2024-11-28 16:49:41 +0100

    p

--- setup.sh
+++ setup.sh
@@ -1,64 +1,96 @@
-# Create directory for Kubernetes apt keyring
-sudo mkdir /etc/apt/keyrings
 
-# Update package lists
+# """""""""""""""""""""""""""""""""""""""""""""" install docker """"""""""""""""""""""""""""""""""""""""""""""""""""""""""
+echo "sudo apt-get update -y"
+sudo apt-get update -y
+
+echo "sudo apt-get install -y ca-certificates curl -y"
+sudo apt-get install -y ca-certificates curl -y
+
+echo "sudo install -m 0755 -d /etc/apt/keyrings"
+sudo install -m 0755 -d /etc/apt/keyrings
+
+echo "sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc"
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+
+echo "sudo chmod a+r /etc/apt/keyrings/docker.asc"
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo "Ajout du dépôt Docker"
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo \"$VERSION_CODENAME\") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+echo "sudo apt-get update -y"
+sudo apt-get update -y
+
+echo "sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y"
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+#echo "sudo usermod -aG docker \$USER && newgrp docker"
+#sudo usermod -aG docker $USER && newgrp docker
+
+if [ "$1" != "--post-newgrp" ]; then
+  echo "Ajout de l'utilisateur actuel au groupe Docker et bascule vers le nouveau groupe"
+  sudo usermod -aG docker $USER
+  exec newgrp docker <<EONG
+$0 --post-newgrp
+EONG
+  exit 0
+fi
+
+# """""""""""""""""""""""""""""""""""""""""""""" install Kubernetes """"""""""""""""""""""""""""""""""""""""""""""""""""""""""
+echo "sudo apt-get update"
 sudo apt-get update
 
-# Install necessary packages for Kubernetes
+echo "sudo apt-get install -y apt-transport-https ca-certificates curl gpg"
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 
-# Download Kubernetes GPG key and add it to apt keyring
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg"
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
-# Add Kubernetes repository to apt sources list
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "Ajout du dépôt Kubernetes"
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-# Update package lists again
+echo "sudo apt-get update"
 sudo apt-get update
 
-# Install Kubernetes packages
+echo "sudo apt-get install -y kubelet kubeadm kubectl"
 sudo apt-get install -y kubelet kubeadm kubectl
 
-# Hold Kubernetes packages to prevent accidental upgrades
+echo "sudo apt-mark hold kubelet kubeadm kubectl"
 sudo apt-mark hold kubelet kubeadm kubectl
 
-# Enable IP forwarding
-# sudo sysctl net.ipv4.conf.all.forwarding=1
+# Désactiver swap
+echo "sudo swapoff -a"
+sudo swapoff -a
 
-# Accept forwarding rules
-# sudo iptables -P FORWARD ACCEPT
+# Configurer containerd
+echo "sudo su <<EOF"
+sudo su <<EOF
+cat > /etc/containerd/config.toml <<EOFF
+[plugins."io.containerd.grpc.v1.cri"]
+  systemd_cgroup = true
+EOFF
+systemctl restart containerd
+exit
+EOF
+
+echo "sudo modprobe br_netfilter"
+sudo modprobe br_netfilter
+
+echo "sudo echo 1 > /proc/sys/net/bridge/bridge-nf-call-iptables"
+sudo echo 1 > /proc/sys/net/bridge/bridge-nf-call-iptables
+
+echo "sudo echo 1 > /proc/sys/net/ipv4/ip_forward"
+sudo echo 1 > /proc/sys/net/ipv4/ip_forward
+
+echo "yes | sudo kubeadm reset"
+yes | sudo kubeadm reset
+
 
 # Disable swap
 sudo swapoff -a
-
-
-
-# Update package lists
-sudo apt-get update
-
-# Install necessary packages
-sudo apt-get install ca-certificates curl
-
-# Create directory for apt keyrings
-sudo install -m 0755 -d /etc/apt/keyrings
-
-# Download Docker GPG key and add it to apt keyring
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add Docker repository to apt sources list
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-# Update package lists again
-sudo apt-get update
-
-# Install Docker packages
-yes | sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-
 
 # Configure containerd
 sudo su <<EOF
